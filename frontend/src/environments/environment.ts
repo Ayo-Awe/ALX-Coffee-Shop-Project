76d88a2bafeb745a8acd518d73d4695c8d15231e
Awe <pupoawe@icloud.com>
2022-07-03 21:03:07 +0100

    feat: implement auth

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "dev-r5yivtq1.us", // the auth0 domain prefix
+    audience: "Coffee", // the audience set for the auth0 app
+    clientId: "xPme98b6DnG8uz0k47gDgdPUj3tnUO2K", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+  },
 };
